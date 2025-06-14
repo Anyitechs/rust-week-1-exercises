7ba6efc2a3e7a1327da551983a36534da9c1fc83
Anyitechs <aifeanyi019@gmail.com>
2025-06-14 17:30:01 +0100

    complete `extract_tx_version` helper function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
-// Implement extract_tx_version function below
+use hex::{decode, FromHexError};
+
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+
+    if raw_tx_hex.len() < 8 {
+        return Err("Transaction data too short".to_string());
+    }
+
+    let tx_byte = match decode(raw_tx_hex) {
+        Ok(tx_byte) => tx_byte,
+        Err(e) => match e {
+            FromHexError::InvalidHexCharacter { .. } => {
+                return Err("Hex decode error".to_string());
+            },
+            FromHexError::OddLength => {
+                return Err("Odd length hex string".to_string());
+            },
+            FromHexError::InvalidStringLength => {
+                return Err("Transaction data too short".to_string());
+            },
+        },
+    };
+
+    let version = tx_byte[0] as u32;
+    Ok(version)
 }
